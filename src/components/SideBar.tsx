e58a37ae67458224765324ad9ba2362d37a3ce70
Arsene1999 <67969013+Arsene1999@users.noreply.github.com>
2021-03-15 14:01:51 -0300

    Update SideBar.tsx

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -20,22 +20,23 @@ export function SideBar(props: SideBarProps) {
     props.setSelectedGenreId(id);
   }
 return(
-<nav className="sidebar">
-<span>Watch<p>Me</p></span>
+  <nav className="sidebar">
+  <span>Watch<p>Me</p></span>
 
-<div className="buttons-container">
-  {props.genres.map((genre: { id: number; title: string; name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'; }) => (
-    <Button
-      id={String(genre.id)}
-      title={genre.title}
-      iconName={genre.name}
-      onClick={() => handleClickButton(genre.id)}
-      selected={props.selectedGenreId === genre.id}
-      />
-      )
-    )}
-  </div>
-</nav>)
+  <div className="buttons-container">
+    {props.genres.map((genre: { id: number; title: string; name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'; }) => (
+      <Button
+        id={String(genre.id)}
+        title={genre.title}
+        iconName={genre.name}
+        onClick={() => handleClickButton(genre.id)}
+        selected={props.selectedGenreId === genre.id}
+        />
+        )
+      )}
+    </div>
+  </nav>
+  )
   
 }
 
